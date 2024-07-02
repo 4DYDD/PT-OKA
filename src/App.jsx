f31adb61f22ee932e941c367f87d2f9ab99baf37
4dydd <haidiraditya16@gmail.com>
2024-07-02 09:46:29 +0800

    ngetes state di github part 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,38 @@ import LoadingScreen from "./components/LoadingScreen";
 
 function App() {
   const [isLoading, setisLoading] = useState(true);
+  const [angka, setAngka] = useState(0);
 
-  useEffect(() => {
-    return () => {
-      setTimeout(() => {
-        setisLoading(false);
-      }, 2000);
-    };
-  }, []);
+  // useEffect(() => {
+  //   return () => {
+  //     setTimeout(() => {
+  //       setisLoading(false);
+  //     }, 5500);
+  //   };
+  // }, []);
 
   return (
     <>
       {isLoading && <LoadingScreen />}
 
       <Header />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <div className="flex">
+        <button
+          className="w-[3rem] py-1 m-auto text-white bg-blue-600 rounded shadow-sm text-lg"
+          onClick={() => {
+            setAngka(angka + 1);
+          }}
+        >
+          {angka}
+        </button>
+      </div>
 
       <Content />
 
